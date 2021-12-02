2418617bbea59e6cddac15cfb76cf5079182c4ac
mangottohaveacode <mbandic@protonmail.com>
2021-12-02 14:16:49 +0100

    Test for Enis

--- jsr.js
+++ jsr.js
@@ -11,7 +11,7 @@ const computerTurn = () => {
     return value.toLowerCase();
 }
 
-const playerTurn = prompt("Choose?");
+let playerTurn = "value";
 let playWin = 0;
 let compWin = 0;
 
@@ -19,49 +19,88 @@ const theGame = (playerChoice, computerSelection) => {
     let winner = "";
     let playerSelection = playerChoice.toLowerCase();
     if(playerSelection === "rock" && computerSelection === "paper"){
-        winner = "computer";
+        winner = "computer ";
         compWin++;
-        return "You lose, paper beats rock! The winner is " + winner; 
+        return winner; 
     }else if(playerSelection === "rock" && computerSelection === "scissors"){
-        winner = "player"
+        winner = " player "
         playWin++;
-        return "You lose, rock beats scissors! The winner is " + winner;  
+        return winner;  
     }else if(playerSelection === "paper" && computerSelection === "scissors"){
-        winner = "computer"
+        winner = " computer "
         compWin++;
-        return "You lose, scissors beats paper! The winner is " + winner; 
+        return winner; 
     }else if (playerSelection === "paper" && computerSelection === "rock"){
-        winner = "player";
+        winner = " player ";
         playWin++;
-        return "You lose, paper beats rock! The winner is " + winner;
+        return winner;
     }else if(playerSelection === "scissors" && computerSelection === "rock"){
-        winner = "computer";
+        winner = " computer ";
         compWin++;
-        return "You lose, rock beats scissors! The winner is " + winner;
+        return winner;
     }else if(playerSelection === "scissors" && computerSelection === "paper"){
-        winner = "player";
+        winner = " player ";
         playWin++;
-        return "You lose, scissors beats paper! The winner is " + winner; 
-    }else {
-        return "Draw"
+        return winner; 
+    }else { 
+        return "draw";
     }
 }
 
+const mybr = document.createElement('br');
 
+const gameDiv = document.querySelector("#game");
+gameDiv.classList.add("buttons");
 
+const button1 = document.createElement("BUTTON");
+gameDiv.appendChild(button1);
+button1.textContent = "ROCK"
+button1.setAttribute("id", "rock")
+// button1.style.border = "1px solid black";
 
+const button2 = document.createElement("BUTTON");
+button2.textContent = "PAPER";
+gameDiv.appendChild(button2);
+button2.setAttribute("id", "paper")
 
-const fiveTimes = () => {
-    for(let i=0; i<5;i++){
-        console.log(theGame(playerTurn, computerTurn()));
-    }
-    if(playWin > compWin){
-        return "The winner is player";
-    }else if(playWin < compWin){
-        return "The winner is computer";
-    }else {
-        return "draw"
-    }
-} 
+const button3 = document.createElement("BUTTON");
+button3.textContent = "SCISSORS";
+gameDiv.appendChild(button3);
+button3.setAttribute("id", "scissors")
 
-console.log(fiveTimes());
+const mainDiv = document.createElement("div");
+mainDiv.classList.add("main");
+document.body.appendChild(mainDiv);
+mainDiv.style.border = "2px solid black";
+
+const resultDiv = document.createElement("div");
+resultDiv.appendChild(mainDiv);
+resultDiv.setAttribute("id", "result") 
+
+let winners = [];
+
+const rock = document.querySelector("#rock");
+rock.addEventListener("click", () => {
+    playerTurn = "rock";
+    let whoWon = (theGame(playerTurn, computerTurn()));
+    const result = whoWon;
+    winners.push(result);
+});
+
+const paper = document.querySelector("#paper");
+paper.addEventListener("click", () => {
+    playerTurn = "paper";
+    let whoWon = (theGame(playerTurn, computerTurn()));
+    const result = whoWon;
+    winners.push(result);
+});
+
+const scissors = document.querySelector("#scissors");
+scissors.addEventListener("click", () => {
+    playerTurn = "scissors";
+    let whoWon = (theGame(playerTurn, computerTurn()));
+    const result = whoWon;
+    winners.push(result);
+});
+
+console.log(winners);
